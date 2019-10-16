9a6514854a0ebc66f5e728cd13ce3c8f95f448d0
Minh Bui <tranduyminh.bui@gmail.com>
2019-10-16 17:21:13 +1030

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Person from "./components/Person";
+import Lonely from "./components/Lonely";
+import data from "./data.json";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = useState([]);
+  const [superLikedUsers, setSuperLikedUsers] = useState([]);
+  const [dislikedUsers, setDislikedUsers] = useState([]);
+  const activeUser = 0;
+
+  switch (key) {
+    case "ADD_TO_LIKED_USER":
+      break;
+    case "ADD_TO_DISLIKED_USER":
+      break;
+    case "ADD_TO_SUPERLIKED_USER":
+      break;
+
+    default:
+      return people;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>hi</h1>
     </div>
   );
 }
